3ac7cfd3a5072d0b594c2c7dd3dd3ddb0a532835
Jay Oh <jungwon.oh0920@gmail.com>
2023-03-12 11:21:55 -0700

    config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: "imgix",
-    path: "https://s3.us-west-2.amazonaws.com/",
+    // loader: "imgix",
+    // path: "https://s3.us-west-2.amazonaws.com/",
     domains: [
       'www.notion.so',
       's3.us-west-2.amazonaws.com/',
